8892776244ba81973c1389e3af704dcb9166e4f0
joao victor <jv224063@gmail.com>
2025-11-24 19:41:20 -0300

    cor do icons arrumado

--- src/NavegationBar.jsx
+++ src/NavegationBar.jsx
@@ -8,12 +8,12 @@ const NavegationBar = () => {
         
         <section className="flex flex-col justify-center items-center space-y-3 pt-5 fixed top-50 left-35 font-bold gap-2 text-lg"> 
 
-            <h3><a href="#Profile" className="hover:text-amber-800 transition-all 2s ">Sobre</a></h3>
-            <h3><a href="#Experiencia"className="hover:text-amber-800 transition-all 2s">Experiencias</a></h3>
-            <h3><a href="#Projects"className="hover:text-amber-800 transition-all 2s">Projetos</a></h3>
-            <h3><a href="#Services"className="hover:text-amber-800 transition-all 2s">Serviços</a></h3>
-            <h3><a href="#Habilidades"className="hover:text-amber-800 transition-all 2s" >Habilidades</a></h3>
-            <h3><a href=""className="hover:text-amber-800 transition-all 2s">Contato</a></h3>
+            <h3><a href="#Profile" className="hover:text-yellow-900 transition-all 2s ">Sobre</a></h3>
+            <h3><a href="#Experiencia"className="hover:text-yellow-900 transition-all 2s">Experiencias</a></h3>
+            <h3><a href="#Projects"className="hover:text-yellow-900 transition-all 2s">Projetos</a></h3>
+            <h3><a href="#Services"className="hover:text-yellow-900 transition-all 2s">Serviços</a></h3>
+            <h3><a href="#Habilidades"className="hover:text-yellow-900 transition-all 2s" >Habilidades</a></h3>
+            <h3><a href=""className="hover:text-yellow-900 transition-all 2s">Contato</a></h3>
             
         </section>
       </div>
