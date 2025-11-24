6474cd2de9b1a043e34c428e5e8c6f5ceaaca6a0
joao victor <jv224063@gmail.com>
2025-11-24 17:52:58 -0300

    arrumando o cor navbar

--- src/NavegationBar.jsx
+++ src/NavegationBar.jsx
@@ -9,7 +9,7 @@ const NavegationBar = () => {
         <section className="flex flex-col justify-center items-center space-y-3 pt-5 fixed top-50 left-35 font-bold gap-2 text-lg"> 
 
             <h3><a href="#Profile" className="hover:text-amber-800 transition-all 2s ">Sobre</a></h3>
-            <h3><a href="#Experiencia"className="hover:text-amber-400 transition-all 2s">Experiencias</a></h3>
+            <h3><a href="#Experiencia"className="hover:text-amber-800 transition-all 2s">Experiencias</a></h3>
             <h3><a href="#Projects"className="hover:text-amber-800 transition-all 2s">Projetos</a></h3>
             <h3><a href="#Services"className="hover:text-amber-800 transition-all 2s">Serviços</a></h3>
             <h3><a href="#Habilidades"className="hover:text-amber-800 transition-all 2s" >Habilidades</a></h3>
