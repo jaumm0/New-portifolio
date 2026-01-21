91a4113ad671c0f3e5fa384a71547cec703b51ed
Jaumm0 <jv224063@gmail.com>
2026-01-21 16:46:56 -0400

    add new project

--- src/Experiencia.jsx
+++ src/Experiencia.jsx
@@ -5,14 +5,11 @@ function Experiencia() {
         <div id="Experiencia" className="flex flex-col mt-50 font-sans  border-yellow-900  ">
             <h3 className="  top-50 text-3xl pb-5 font-bold flex ">Experiências</h3>
             <ul className=" top-60 list-disc space-y-4 border justify-center rounded-2xl p-10  border-yellow-900">
-                <li className=" font-bold ">Desenvolvedor Front-end na Tech Solutions (2022 - Presente)
-                    <p className=" font-normal ">Responsável pelo desenvolvimento de interfaces de usuário responsivas utilizando React.js e Tailwind CSS. Colaborei com designers para transformar wireframes em componentes reutilizáveis, melhorando a experiência do usuário em 30%.</p>
+                <li className=" font-bold ">Auxiliar de TI — (1 mês – Atual)
+                    <p className=" font-normal ">Atuação no suporte e apoio técnico, com envolvimento em atividades relacionadas à tecnologia e desenvolvimento de sistemas. Auxílio na manutenção de sistemas, configuração de ambientes, testes básicos de aplicações e apoio na resolução de problemas técnicos. Contato com lógica de programação, organização de código e boas práticas de tecnologia, contribuindo para o desenvolvimento de habilidades voltadas ao desenvolvimento front-end.</p>
                 </li>
-                <li className=" font-bold ">Estagiário em Desenvolvimento Web na Web Innovators (2021 - 2022)
-                    <p className=" font-normal ">Apoiei a equipe de desenvolvimento na criação de páginas web dinâmicas usando HTML, CSS e JavaScript. Participei de reuniões de sprint e contribuí para a resolução de bugs, adquirindo experiência prática em metodologias ágeis.</p>
-                </li>
-                <li className=" font-bold ">Projetos Freelance (2020 - Presente)
-                    <p class=" font-normal ">Desenvolvi diversos projetos freelance para pequenos negócios, incluindo sites institucionais e lojas online. Utilizei React para construir interfaces interativas e implementei soluções de back-end simples com Node.js, garantindo a satisfação do cliente.</p>
+                <li className=" font-bold ">Projeto Pessoal – Buscador de Perfis do GitHub
+                    <p className=" font-normal ">Desenvolvimento de uma aplicação front-end utilizando React.js e Tailwind CSS, integrada à GitHub REST API, permitindo a busca de usuários do GitHub e a visualização de informações como nome, foto de perfil, seguidores, quantidade de repositórios e listagem de repositórios com nome e descrição.</p>
                 </li>
             </ul>       
         </div>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,30 +1,31 @@
 import { useState, useEffect } from "react";
 import { ArrowBigRightDash } from 'lucide-react';
 import { ArrowBigLeftDash } from 'lucide-react';
-import Assets from "./Assets/test.png";
+import Assets from "./Assets/DevStack.png";
+import Psicologo from "./Assets/Psicologo.png";
 
 function Projects() {
   const projects = [
     {
-      title: "Landing Page ",
+      title: "Search Github Profile",
+      image: Assets,
+      link: "https://jaumm0.github.io/SeachProfilleGitHubAPI/",
+      description:
+      "Projeto desenvolvido em React.js com Tailwind CSS, utilizando a GitHub REST API para buscar e exibir informações de usuários do GitHub. A aplicação permite visualizar dados do perfil, como foto, seguidores, número de repositórios, além da listagem detalhada de repositórios com nome e descrição. O projeto foca em componentização, consumo de APIs REST e criação de uma interface responsiva e moderna."
+    },
+    {
+      title: "Landing Page",
       image: "https://acdn-us.mitiendanube.com/stores/001/929/213/themes/atlantico/2-slide-1734049808011-3571780959-ac8720bc9fe5a2c6e971c0536ef96b651734049810-1920-1920.webp?272675730",
       link: "https://jaumm0.github.io/Landing-page-react/",
       description:
         "Landing page desenvolvida com foco na venda de produtos gamer de alta qualidade, priorizando performance, design moderno e experiência do usuário. O projeto foi construído com React e Vite para garantir carregamento rápido e uma estrutura eficiente, Tailwind CSS para estilização responsiva e consistente, e Lucide Icons para ícones leves e modernos. O layout foi pensado para destacar os produtos, reforçar a identidade gamer e incentivar a conversão.",
     },
     {
-      title: "Portal de Notícias em Tempo Real",
-      image: Assets,
-      link: "https://jaumm0.github.io/Landing-page-react/",
+      title: "Landing Page Psicóloga Freelancer",
+      image: Psicologo,
+      link: "https://jaumm0.github.io/Page-psicologa-freela/#Contact",
       description:
-        "Portal de notícias que consome APIs públicas para exibir notícias em tempo real, com React no front-end.",
-    },
-    {
-      title: "Loja Online de Produtos Sustentáveis",
-      image: "https://via.placeholder.com/600x300",
-      link: "https://jaumm0.github.io/Landing-page-react/",
-      description:
-        "E-commerce focado em produtos sustentáveis, com carrinho de compras e integração com pagamentos.",
+        "Desenvolvido como freelancer, este projeto consiste em uma landing page moderna e responsiva para psicólogo, construída com React 19.2.0 para criação de interfaces dinâmicas, Vite 7.2.4 como ferramenta de build e servidor de desenvolvimento, e estilizada com Tailwind CSS 4.1.18 para um design limpo e funcional. Foram utilizados ícones SVG do Lucide React 0.562.0 e implementadas boas práticas de código com ESLint 9.39.1. O projeto foi publicado com sucesso no GitHub Pages (gh-pages 6.3.0), garantindo fácil acesso online.",
     },
   ];
 
